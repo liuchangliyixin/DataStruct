ee5ba6f2e814c97e1a33d7edfbb35f39e2fb8150
yixiu <honghuo12345@163.com>
2020-02-25 23:54:15 +0800

    项目初始化

---
+++ src/model/SingleLinkNode.java
@@ -0,0 +1,13 @@
+package model;
+
+public class SingleLinkNode {
+    private int val;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+}
